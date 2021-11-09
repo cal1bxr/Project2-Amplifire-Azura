83e49acbd97c94392af7b66a4199fbbc0f945853
Cal1bxr <mjordandevelop@gmail.com>
2021-11-09 17:29:26 -0500

    Login compont now redirecting

--- Angular/matchify/src/app/components/login/login.component.ts
+++ Angular/matchify/src/app/components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { LoginService } from 'src/app/services/login.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private loginService: LoginService) { }
 
   ngOnInit(): void {
   }
 
+  onClick(){
+    console.log(this.loginService.getToken());
+    console.log(window.location.href)
+    window.location.href = "https://accounts.spotify.com/authorize?client_id=5f137a45a0d1403bb3e9a31625282aed&response_type=token&scope=user-read-private&redirect_uri=http://localhost:4200/home&show_dialog=false"
+    console.log("Did it make it");
+  }
 }
