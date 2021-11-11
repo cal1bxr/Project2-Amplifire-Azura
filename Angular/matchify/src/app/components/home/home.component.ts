afb4c64d7279a00bfadc20f07ccfb96bf97feea7
Cal1bxr <mjordandevelop@gmail.com>
2021-11-10 20:53:19 -0500

    OAuth finished for authorization with Spotify

--- Angular/matchify/src/app/components/home/home.component.ts
+++ Angular/matchify/src/app/components/home/home.component.ts
@@ -1,18 +1,8 @@
-import { HttpClient, HttpErrorResponse, HttpHeaders, HttpParams } from '@angular/common/http';
-import { Component, ComponentFactoryResolver, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { HttpErrorResponse } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 import { throwError } from 'rxjs';
-import { catchError, map } from 'rxjs/operators';
 
-
-const ACCESSTOKENURL = 'https://accounts.spotify.com/api/token';
-const redirectUri = 'http://localhost:4200/home';
-const clientId = 'bd65c2a1c20245dda47c526d5fb90a2b';
-const clientSecret = '8380aa9e99324cfc8477e329b444f3ff';
-let code:string = '';
-
-
-
+import { LoginService } from 'src/app/services/login.service';
 
 
 @Component({
@@ -22,7 +12,6 @@ let code:string = '';
 })
 export class HomeComponent implements OnInit {
 
-  redirect_uri = redirectUri;
   private static handleError(error: HttpErrorResponse): any {
     if(error.error instanceof ErrorEvent){
       console.log("Something went wrong", error.error.message);
@@ -35,44 +24,13 @@ export class HomeComponent implements OnInit {
     return throwError("Please Try again later");
   }
   
-  constructor(private router: ActivatedRoute, private http:HttpClient) { }
+  constructor(private loginService: LoginService) { }
 
   ngOnInit(): void { 
-    let win = window.location.href;
-    let winSplit = win.split('=', 2);
-    code = winSplit[1];
-    console.log(code);
-    localStorage.setItem('code', code);
-    // let authOptions = {
-    //   form: {
-    //     code: code,
-    //     redirect_uri: redirectUri,
-    //     'grant_type': 'authorization_code'
-    //   }
-    // };
-  //   let headers = new HttpHeaders()
-  // .set('Content-Type', 'application/x-www-form-urlencoded')
-  // .set('Authorization', 'Basic ' + btoa(clientId + ':' + clientSecret));
-  const httpOptions: { headers: any; observe: any; } = {
-    headers: new HttpHeaders()
-    .set('Content-Type', 'application/x-www-form-urlencoded')
-    .set('Authorization', 'Basic ' + btoa(clientId + ':' + clientSecret)),
-    observe: 'response'
-  };
-  const body = new HttpParams()
-.set('code', code)
-.set('redirect_uri', redirectUri)
-.set('grant_type', 'authorization_code');
-
-   this.http.post('https://accounts.spotify.com/api/token', body, httpOptions).subscribe()
-    //  map((data: any)=> {
-    //    console.log(data);
-    //    return data;
-    //  }), catchError(error => { 
-    //    console.log(error)
-    //    return throwError('Something went wrong');
-    // })
+    this.loginService.getToken();
   }
+
+  
 }  
 
 
--- Angular/matchify/src/app/components/login/login.component.ts
+++ Angular/matchify/src/app/components/login/login.component.ts
@@ -1,11 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
 import { LoginService } from 'src/app/services/login.service';
-
-const clientId = 'bd65c2a1c20245dda47c526d5fb90a2b';
-const redirectUri = 'http://localhost:4200/home';
-const AUTHORIZE = 'https://accounts.spotify.com/authorize';
-const SCOPES: string = "user-read-private%20user-read-recently-played%20user-follow-read%20user-top-read%20user-read-email";
 
 
 @Component({
@@ -15,21 +9,13 @@ const SCOPES: string = "user-read-private%20user-read-recently-played%20user-fol
 })
 export class LoginComponent implements OnInit {
   code: string | undefined;
-  constructor(private loginService: LoginService, private router: ActivatedRoute) { }
+  constructor(private loginService: LoginService) { }
 
   ngOnInit(): void {
     
   }
 
   onClick(){
-    let url = AUTHORIZE;
-    url += '?client_id='+ clientId;
-    url += '&response_type=code';
-    url += '&redirect_uri='+ redirectUri;
-    url += '&show_dialog=true';
-    url += '&scope=' + SCOPES;
-    console.log(url);
-    
-    window.location.href = url;    
+    this.loginService.getAuthorization();
   }
 }
