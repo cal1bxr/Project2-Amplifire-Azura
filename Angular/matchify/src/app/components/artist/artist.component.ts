e597dfddd755f1bac5315c8f88475dca9a4a7735
Albert Li <albertlixw@gmail.com>
2021-11-15 22:12:42 -0500

    artistServices and component is online, functionalities work so far

--- Angular/matchify/src/app/components/artist/artist.component.ts
+++ Angular/matchify/src/app/components/artist/artist.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ArtistService } from 'src/app/services/artist.service';
+import { LoginService } from 'src/app/services/login.service';
 
 @Component({
   selector: 'app-artist',
@@ -7,9 +9,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ArtistComponent implements OnInit {
 
-  constructor() { }
+  artists: any = '';
+  constructor(private loginService: LoginService, private artistService: ArtistService) { }
 
   ngOnInit(): void {
+    this.loginService.getRefreshToken(this.loginService.accessToken, this.loginService.refreshToken, this.loginService.code);
+    this.artistService.getArtists().subscribe((response) => {this.artists=response;})
+
+    for(let artist of this.artists){
+      console.log(artist);
+    }
   }
 
 }
